4550983833dec91811a56157f5cb9279b22996c3
Valdas <116576563+ValdasKa@users.noreply.github.com>
2023-06-05 12:38:02 +0000

    Fix house_controller

--- app/controller/house_controller.lua
+++ app/controller/house_controller.lua
@@ -1,11 +1,21 @@
 local house_controller = {}
 
 function house_controller.Get()
+    HttpResponseCode:send200()
     return "house controller GET reached"
 end
 function house_controller.Post()
+    HttpResponseCode:send200()
     return "house controller POST reached"
 end
--- veliau prideti kitus 
+function house_controller.Delete()
+    HttpResponseCode:send200()
+    return "house controller DELETE reached"
+end
+function house_controller.Put()
+    HttpResponseCode:send200()
+    return "house controller PUT reached"
+end
+
 
 return house_controller
