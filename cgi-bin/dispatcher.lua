79acbefeb87057130689c651595410f6a46f4fc0
Valdas <116576563+ValdasKa@users.noreply.github.com>
2023-06-06 07:54:46 +0000

    Try make router work

--- cgi-bin/dispatcher.lua
+++ cgi-bin/dispatcher.lua
@@ -6,17 +6,28 @@ require("utils.contains")
 local RequestParser = require("utils.parser")
 
 local function send_response(response)
-    uhttpd.send("Status: 200\r\n")
-    uhttpd.send("Content-Type: application/json\r\n\r\n")
-    uhttpd.send(response)
+    -- uhttpd.send("Status: 200\r\n")
+    -- uhttpd.send("Content-Type: application/json\r\n\r\n")
+    -- uhttpd.send(response)
+    uhttpd.send(getset:GetInput("status"))
+    getset:SetInput("headers", {})
+    table.insert(getset:GetInput("headers"),"Content-Type:application/json\n")
+    table.insert(getset:GetInput("headers"),"Cache-Control: no-cache\n\n")
+    uhttpd.send("\n" .. cjson.encode(response))
+    uhttpd.send(cjson.encode/(getset:GetInput("env")))
 end
 -- Main body required by uhhtpd-lua plugin
 function handle_request(env)
-    local response
-    if env["REQUEST_METHOD"] == 'GET' then response = dofile("www/app/controller/house_controller.lua").Get() end
-    if env["REQUEST_METHOD"] == 'POST' then response = dofile("www/app/controller/house_controller.lua").Post() end
-    if env["REQUEST_METHOD"] == 'PUT' then response = dofile("www/app/controller/house_controller.lua").Put() end
-    if env["REQUEST_METHOD"] == 'DELETE' then response = dofile("www/app/controller/house_controller.lua").Delete() end
-    send_response(response .. "\n")
-        uhttpd.send(cjson.encode(env))
+    if (RequestParser.ParseRequest(env)) then
+        local response = router.SetPath(getset:GetInput("path"), getset:GetInput("env").REQUEST_METHOD)
+    send_response(response)
+	end
+    
+    -- local response
+    -- if env["REQUEST_METHOD"] == 'GET' then response = dofile("www/app/controller/house_controller.lua").Get() end
+    -- if env["REQUEST_METHOD"] == 'POST' then response = dofile("www/app/controller/house_controller.lua").Post() end
+    -- if env["REQUEST_METHOD"] == 'PUT' then response = dofile("www/app/controller/house_controller.lua").Put() end
+    -- if env["REQUEST_METHOD"] == 'DELETE' then response = dofile("www/app/controller/house_controller.lua").Delete() end
+    -- send_response(response .. "\n")
+    --     uhttpd.send(cjson.encode(env))
 end
