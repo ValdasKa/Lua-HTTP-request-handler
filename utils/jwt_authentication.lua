f6d301c282fc8fb0d5f0c7f1189db8449bb463bd
Valdas <116576563+ValdasKa@users.noreply.github.com>
2023-06-12 11:36:42 +0000

    Small changes to jwt

--- utils/jwt_authentication.lua
+++ utils/jwt_authentication.lua
@@ -1,10 +1,21 @@
-local jwt = {}
-local jwtToken = "JWT_Token"
-local algSign = "HS512"
-local algVerify = "RS512"
+local jwtauth = {}
+
+local key = "example_key"
+local payload = {
+	iss = "12345678",
+	nbf = os.time(),
+	exp = os.time() + 3600,
+}
+-- encode
+local alg = "HS256" -- (default)
+function jwtauth.Encode()
+	local token, err = Jwt.encode(payload, key, alg)
+end
+function jwtauth.Decode(token)
+	local decoded, err = Jwt.verify(token, alg, key)
+	return decoded
+end
 
 
 
-
-
-return jwt
+return jwtauth
