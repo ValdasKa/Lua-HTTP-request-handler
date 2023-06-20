676e828bc2846bdd7e4c5dc7195d0de6af66d66b
Valdas <116576563+ValdasKa@users.noreply.github.com>
2023-06-20 07:37:07 +0000

    Old changes to controller

--- app/controller/house_controller.lua
+++ app/controller/house_controller.lua
@@ -1,9 +1,56 @@
 local house_controller = {}
 
-function house_controller.Get()
-    HttpResponseCode:send200()
-    return "house controller GET reached"
+-- check table exist? for later
+
+-- add print table test 
+
+local sqlite3 = require("luasql.sqlite3")
+
+path = dofile("www/app/database/db.lua").path
+
+
+function house_controller.letsgo()
+local env = assert(sqlite3.sqlite3()) -- create the context
+local conn = assert(env:connect(path)) -- connect to the DB
+local cursor = assert(conn:execute("SELECT * FROM house"))
+local row = {}
+while cursor:fetch(row) do -- print all elements
+   print(table.concat(row, ' | '))
+   uhttpd.send(table.concat(row, ' | '))
 end
+cursor:close() -- close the record fetching handler
+conn:close()  -- close the connection to the DB
+env:close() -- release the context
+end
+
+
+-- path = dofile("www/app/database/db.lua").path
+
+function house_controller.Get()
+--     local env = assert(sqlite3.sqlite3()) -- create the context
+-- local conn = assert(env:connect(path)) -- connect to the DB
+-- local cursor = assert(conn:execute("SELECT * FROM house"))
+-- local row = {}
+-- while cursor:fetch(row) do -- print all elements
+--    print(table.concat(row, ' | '))
+-- --    uhttpd.send(table.concat(row, ' | '))
+-- end
+-- cursor:close() -- close the record fetching handler
+-- conn:close()  -- close the connection to the DB
+-- env:close() -- release the context
+    -- print(path)
+    -- print("\n")
+    -- getset:GetInput("body")
+    HttpResponseCode:send200()
+    -- house_controller.letsgo()
+    return "house controller GET reached"
+   
+end
+house_controller.Get()
+-- function house_controller.Get()
+--         HttpResponseCode:send200()
+--     return "house controller GET reached"
+-- end
 function house_controller.Post()
     HttpResponseCode:send200()
     return "house controller POST reached"
@@ -16,6 +63,4 @@ function house_controller.Put()
     HttpResponseCode:send200()
     return "house controller PUT reached"
 end
-
-
 return house_controller
