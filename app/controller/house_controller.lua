4d2b8c67f1a09c7aa4c709ab8c3e625252cf8080
Valdas <116576563+ValdasKa@users.noreply.github.com>
2023-06-21 05:26:37 +0000

    Print data from db

--- app/controller/house_controller.lua
+++ app/controller/house_controller.lua
@@ -27,31 +27,33 @@ end
 -- path = dofile("www/app/database/db.lua").path
 
 function house_controller.Get()
---     local env = assert(sqlite3.sqlite3()) -- create the context
--- local conn = assert(env:connect(path)) -- connect to the DB
--- local cursor = assert(conn:execute("SELECT * FROM house"))
+    local env = assert(sqlite3.sqlite3()) -- create the context
+local conn = assert(env:connect(path)) -- connect to the DB
+local cursor = assert(conn:execute("SELECT * FROM house"))
 -- local row = {}
--- while cursor:fetch(row) do -- print all elements
---    print(table.concat(row, ' | '))
--- --    uhttpd.send(table.concat(row, ' | '))
--- end
--- cursor:close() -- close the record fetching handler
--- conn:close()  -- close the connection to the DB
--- env:close() -- release the context
+local row = cursor:fetch ({}, "a")
+while row do -- print all elements
+    print((string.format("Id: %s, street: %s, house_number: %s, apartment: %s", row.id, row.street, row.house_number, row.apartment)))
+    -- print(table.concat(row, ' | '))
+--    uhttpd.send(table.concat(row, ' | '))
+    row = cursor:fetch (row, "a")
+end
+cursor:close() -- close the record fetching handler
+conn:close()  -- close the connection to the DB
+env:close() -- release the context
     -- print(path)
-    -- print("\n")
-    -- getset:GetInput("body")
+    print("\n")
+    Payload:GetInput("body")
     HttpResponseCode:send200()
     -- house_controller.letsgo()
     return "house controller GET reached"
    
 end
-house_controller.Get()
+-- house_controller.Get()
 
 function house_controller.Post()
     HttpResponseCode:send200()
-    -- print data-body
-    return Payload:GetInput("data-body") 
+    return Payload:GetInput("data-body")
 end
 function house_controller.Delete()
     HttpResponseCode:send200()
